d906766f9318e64debb522fdd083c4649c15221f
abelko <esung1129@gmail.com>
2018-03-21 10:56:53 +0900

    NodeJS를 이용한 API 서버만들기 3

---
+++ webframeworks/nodejs-api-tutorial1/api/user/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./user.controller');
+
+router.get('/', controller.index);
+
+router.get('/:id', controller.show);
+
+router.post('/', controller.create);
+
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
--- webframeworks/nodejs-api-tutorial1/app.js
+++ webframeworks/nodejs-api-tutorial1/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/users', require('./api/user'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!\n');
- });
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
