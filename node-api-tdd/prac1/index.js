81212594e8275fd044bfe08df967504c2010bcf9
abelko <esung1129@gmail.com>
2018-03-27 22:46:57 +0900

    bodyParser 모듈

--- node-api-tdd/prac1/index.js
+++ node-api-tdd/prac1/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var users = [
   {id: 1, name: 'alice'},
   {id: 2, name: 'bek'},
   {id: 3, name: 'chris'}
 ];
 
+
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/users', function (req, res) {
   req.query.limit = req.query.limit || 10;
@@ -34,6 +38,14 @@ app.delete('/users/:id', function (req, res) {
 
   users = users.filter(user => user.id !== id);
   res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+  const name = req.body.name;
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
 });
 
 app.listen(3000, function() {
