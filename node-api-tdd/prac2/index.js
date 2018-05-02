e7f6762e625b07f4b28b1d3cbd5e218ed5c93727
Abel ko <esung1129@gmail.com>
2018-05-02 09:28:37 +0900

    API get users with limit

--- node-api-tdd/prac2/index.js
+++ node-api-tdd/prac2/index.js
@@ -4,17 +4,23 @@ let morgan = require('morgan');
 let users = [
     {id: 1, name: 'kim sung jun'},
     {id: 2, name: 'hwang ju chan'},
-    {id: 3, name: 'kim jae yun'}
+    {id: 3, name: 'kim jae yun'},
+    {id: 4, name: 'abel ko'}
 ];
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
-})
-
 app.get('/users', (req, res) => {
-    res.json(users); // .json의 역할은 뭐징? 파싱?(파싱은 뭐지?) .json은 배열로도 보낼 수 있나?
+    req.query.limit = req.query.limit || users.length
+    const limit = parseInt(req.query.limit, 10)
+    if(Number.isNaN(limit)) {
+        res.status(400).end();
+    } 
+    
+    const newUsers = [ ...users.slice(0, limit) ];
+
+    //users 보다 limit이 클 때는...?, -1 도 안됨!
+    res.json(newUsers); // .json의 역할은 뭐징? 파싱?(파싱은 뭐지?) .json은 배열로도 보낼 수 있나?
 })
 
 app.post('/users', (req,res) => {
