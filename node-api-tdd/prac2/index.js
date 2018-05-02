fb1b84e106741963e34501e256e1cb11bc4c27b2
Abel ko <esung1129@gmail.com>
2018-05-02 21:10:54 +0900

    sync index controler 1

--- node-api-tdd/prac2/index.js
+++ node-api-tdd/prac2/index.js
@@ -2,26 +2,27 @@ let express = require('express');
 let app = express();
 let morgan = require('morgan');
 const syncDb = require('./bin/sync-db');
-let users = [
-    {id: 1, name: 'kim sung jun'},
-    {id: 2, name: 'hwang ju chan'},
-    {id: 3, name: 'kim jae yun'},
-    {id: 4, name: 'abel ko'}
-];
+// let users = [
+//     {id: 1, name: 'kim sung jun'},
+//     {id: 2, name: 'hwang ju chan'},
+//     {id: 3, name: 'kim jae yun'},
+//     {id: 4, name: 'abel ko'}
+// ];
+const models = require('./models');
 
 app.use(morgan('dev'));
 
 app.get('/users', (req, res) => {
-    req.query.limit = req.query.limit || users.length
+    req.query.limit = req.query.limit || 10
     const limit = parseInt(req.query.limit, 10)
     if(Number.isNaN(limit)) {
         res.status(400).end();
     } 
-    
-    const newUsers = [ ...users.slice(0, limit) ];
 
-    //users 보다 limit이 클 때는...?, -1 도 안됨!
-    res.json(newUsers); // .json의 역할은 뭐징? 파싱?(파싱은 뭐지?) .json은 배열로도 보낼 수 있나?
+    models.User.findAll({})
+        .then(users => {
+            res.json(users)
+        })
 })
 
 app.post('/users', (req,res) => {
